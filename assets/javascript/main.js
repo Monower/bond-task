2ca6e52551b614bfd6e9667ebedca167783c7c6c
monower <sadafahmed720@gmail.com>
2023-12-25 23:18:44 +0600

    worked on the navbar

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+
+const toggleDropdown = () => {
+    const dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "block";
+    }
+}
